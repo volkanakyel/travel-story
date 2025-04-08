65401a9fae1c78da1cdd450a03afa26c37dafa43
Volkan Akyel <volkan@uikielle.com>
2025-04-08 14:53:47 +0200

    update & install base setup dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@nuxt/image', 'motion-v/nuxt', '@nuxt/eslint', '@nuxtjs/tailwindcss'],
 })
